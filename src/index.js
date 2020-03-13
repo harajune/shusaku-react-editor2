aa2e14158b3ecfa3061096bfb95cf487430719e8
harajune <harajune@gijutsuya.jp>
2020-03-13 22:12:01 +0900

    integrate ckeditor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
+
 import './index.css';
 
 
 class App extends React.Component {
   render() {
     return (
-      <div>helloworld</div>
+      <div>
+        <CKEditor
+          editor={BalloonEditor}
+          data="<p>Hello from CKEditor</p>"
+          onInit={
+            editor => {
+              editor.keystrokes.set('Tab', (data, cancel) => {
+                editor.model.change( writer => {
+                  // cursor position
+                  const insertPosition = editor.model.document.selection.getFirstPosition();
+                  const column = insertPosition.path[1];
+                  writer.insertText( '    ', insertPosition );              
+                });
+                cancel();
+              });
+            }
+          }
+          onChange={(event, editor) => {
+            const data = editor.getData();
+          }}
+          onBlur={(event, editor) => {
+          }}
+          onFocus={(event, editor) => {
+          }}
+
+        />
+
+      </div>
     );
   }
 }
